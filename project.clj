287691a83dedf6ef3d566179e542483b102caad0
stask <stask@me.com>
2017-07-18 17:27:00 +0300

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject docker-client "0.1.0-SNAPSHOT"
+(defproject docker-client "0.1.0"
   :description "Clojure wrapper for Spotify's docker-client library"
   :url "https://github.com/stask/docker-client"
   :license {:name "Eclipse Public License"
