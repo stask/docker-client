b0921795a5819144e0b6e4f50cfc49c034ea5358
stask <stask@me.com>
2017-07-18 17:25:08 +0300

    client creation and list containers

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/stask/docker-client"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure       "1.8.0"]
+                 [com.spotify/docker-client "8.8.1"]])
--- src/docker_client/core.clj
+++ src/docker_client/core.clj
@@ -1,6 +1,20 @@
-(ns docker-client.core)
+(ns docker-client.core
+  (:import
+   (com.spotify.docker.client DockerClient
+                              DockerClient$ListContainersParam
+                              DefaultDockerClient)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn make-client
+  ([]
+   (.build (DefaultDockerClient/fromEnv)))
+  ([url]
+   (DefaultDockerClient. url)))
+
+(defn list-containers
+  ([] (list-containers false))
+  ([all?]
+   (let [params (into-array DockerClient$ListContainersParam
+                            (if all?
+                              [(DockerClient$ListContainersParam/allContainers)]
+                              []))]
+     (.listContainers client params))))
